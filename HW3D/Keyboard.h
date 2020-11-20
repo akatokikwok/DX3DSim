358c0dc96ba41a3fede41fb69a6042d30fb582b8
akatokikwok <guorenbin30@gmail.com>
2020-11-20 22:39:51 +0800

    none

--- HW3D/Keyboard.h
+++ HW3D/Keyboard.h
@@ -1,1 +1,52 @@
 #pragma once
+#include <queue>
+#include <bitset>
+#include "Window.h"
+
+class Keyboard
+{
+	friend class Window;
+public:
+	class Event
+	{
+	public:
+		enum class Type
+		{
+			Press,
+			Release,
+			Invalid
+		};
+	private:
+		Type type;
+		unsigned char code;
+	public:
+		Event()
+			:
+			type(Type::Invalid),
+			code(0u)
+		{}
+		Event(Type type, unsigned char code) noexcept
+			:
+			type(type),
+			code(code)
+		{}
+		bool IsPress() const noexcept
+		{
+			return type == Type::Press;
+		}
+		bool IsRelease() const noexcept
+		{
+			return type == Type::Release;
+		}
+		bool IsValid() const noexcept
+		{
+			return type != Type::Invalid;
+		}
+		unsigned char GetCode() const noexcept
+		{
+			return code;
+		}		
+	};
+protected:
+private:
+};
