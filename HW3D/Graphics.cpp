4798f93374a9495aefe89a2b683da9cfb1718903
akatokikwok <guorenbin30@gmail.com>
2021-01-02 17:20:52 +0800

    Graphics类的抛出的宏进行添加与改写

--- HW3D/Graphics.cpp
+++ HW3D/Graphics.cpp
@@ -1,6 +1,22 @@
 ﻿#include "Graphics.h"
+#include "dxerr.h"
+#include <sstream>
 
 #pragma comment(lib, "d3d11.lib")//链接到库
+
+// graphics exception checking/throwing macros (some with dxgi infos)
+#define GFX_EXCEPT_NOINFO(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_NOINFO(hrcall) if( FAILED( hr = (hrcall) ) ) throw Graphics::HrException( __LINE__,__FILE__,hr )
+
+#ifndef NDEBUG
+#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
+#define GFX_THROW_INFO(hrcall) infoManager.Set(); if( FAILED( hr = (hrcall) ) ) throw GFX_EXCEPT(hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
+#else
+#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr) )
+#endif
 
 Graphics::Graphics(HWND hWnd)
 {
@@ -21,11 +37,20 @@ Graphics::Graphics(HWND hWnd)
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;//性能最高
 	sd.Flags = 0;
 
-	D3D11CreateDeviceAndSwapChain(
+	/* 设备层开启调试*/
+	UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+	swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	// for checking results of d3d functions
+	HRESULT hr;
+
+	GFX_THROW_INFO(D3D11CreateDeviceAndSwapChain(
 		nullptr,//默认显卡 
 		D3D_DRIVER_TYPE_HARDWARE,//使用硬件设备 
 		nullptr,//软件驱动 
-		0,//扩展标签 
+		swapCreateFlags,//扩展标签 
 		nullptr,//特性级别 
 		0,
 		D3D11_SDK_VERSION,
@@ -34,14 +59,14 @@ Graphics::Graphics(HWND hWnd)
 		&pDevice,//指向设备的指针
 		nullptr,//输出特性级别
 		&pContext	//上下文
-	);
+	));
 
 	//用来保存交换链里后缓存
 	ID3D11Resource* pBackBuffer = nullptr;
 	//用交换链的方法访问后台缓存纹理
-	pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer));
+	GFX_THROW_INFO(pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer)));
 	//用获取到的纹理来创建渲染目标视图
-	pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &pTarget);
+	GFX_THROW_INFO(pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &pTarget));
 	//具备了渲染视图之后可以释放后台缓存
 	pBackBuffer->Release();
 }
@@ -69,6 +94,95 @@ Graphics::~Graphics()
 
 void Graphics::EndFrame()
 {
-	pSwap->Present(/*同步间隔*/1u, 0u);
+	HRESULT hr;
+#ifndef NDEBUG
+	infoManager.Set();
+#endif
+
+	if (FAILED(hr = pSwap->Present(1u, 0u)))
+	{
+		if (hr == DXGI_ERROR_DEVICE_REMOVED)
+		{
+			throw GFX_DEVICE_REMOVED_EXCEPT(pDevice->GetDeviceRemovedReason());
+		}
+		else
+		{
+			throw GFX_EXCEPT(hr);
+		}
+
+		//pSwap->Present(/*同步间隔*/1u, 0u);
+	}
 }
 
+void Graphics::ClearBuffer(float red, float green, float blue) noexcept
+{
+	//给一个颜色
+	const float color[] = { red, green, blue, 1.0f };
+	//在上下文以指定的颜色来填充清除渲染视图
+	pContext->ClearRenderTargetView(pTarget, color);
+}
+
+/// 各异常类实现 //////////////////////////////////////////////////////////////////////////
+Graphics::HrException::HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs) noexcept
+	:
+	Exception(line, file),
+	hr(hr)
+{
+	// join all info messages with newlines into single string
+	for (const auto& m : infoMsgs)
+	{
+		info += m;
+		info.push_back('\n');
+	}
+	// remove final newline if exists
+	if (!info.empty())
+	{
+		info.pop_back();
+	}
+}
+
+const char* Graphics::HrException::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< "[Error String] " << GetErrorString() << std::endl
+		<< "[Description] " << GetErrorDescription() << std::endl;
+	if (!info.empty())
+	{
+		oss << "\n[Error Info]\n" << GetErrorInfo() << std::endl << std::endl;
+	}
+	oss << GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Graphics::HrException::GetType() const noexcept
+{
+	return "Grb Graphics Exception";
+}
+HRESULT Graphics::HrException::GetErrorCode() const noexcept
+{
+	return hr;
+}
+std::string Graphics::HrException::GetErrorString() const noexcept
+{
+	return DXGetErrorString(hr);
+}
+std::string Graphics::HrException::GetErrorDescription() const noexcept
+{
+	char buf[512];
+	DXGetErrorDescription(hr, buf, sizeof(buf));
+	return buf;
+}
+
+std::string Graphics::HrException::GetErrorInfo() const noexcept
+{
+	return info;
+}
+
+const char* Graphics::DeviceRemovedException::GetType() const noexcept
+{
+	return "Grb Graphics Exception [Device Removed] (DXGI_ERROR_DEVICE_REMOVED)";
+}
--- HW3D/Graphics.h
+++ HW3D/Graphics.h
@@ -1,10 +1,43 @@
 ﻿#pragma once
-
+#include "GrbException.h"
 #include "GrbWin.h"
 #include <d3d11.h>
+#include <vector>
+#include "DxgiInfoManager.h"
 
 class Graphics
 {
+public:
+	/* 异常类*/
+	class Exception : public GrbException
+	{
+		using GrbException::GrbException;
+	};
+	/* HR异常类*/
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs = {}) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
+	private:
+		HRESULT hr;
+		std::string info;
+	};
+	/* 设备被意外移除异常类*/
+	class DeviceRemovedException : public HrException
+	{
+		using HrException::HrException;
+	public:
+		const char* GetType() const noexcept override;
+	private:
+		std::string reason;
+	};
+/// //////////////////////////////////////////////////////////////////////////
 public:
 	Graphics(HWND hWnd);
 	/* 禁用拷贝和复制带参构造*/
@@ -14,16 +47,12 @@ public:
 	/*每帧结束瞬间做的事*/
 	void EndFrame();
 	/*清除渲染视图的进一步封装方法*/
-	void ClearBuffer(float red, float green, float blue) noexcept
-	{
-		//给一个颜色
-		const float color[] = {red, green, blue, 1.0f};
-		//在上下文以指定的颜色来填充清除渲染视图
-		pContext->ClearRenderTargetView(pTarget, color);
-	};
+	void ClearBuffer(float red, float green, float blue) noexcept;
 
-protected:
 private:
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif
 	ID3D11Device* pDevice = nullptr;//设备
 	IDXGISwapChain* pSwap = nullptr;//交换链
 	ID3D11DeviceContext* pContext = nullptr;//上下文
