9faa71f01c3cb3f829f9afab3e6ba8830f362e7f
akatokikwok <guorenbin30@gmail.com>
2021-01-02 15:45:51 +0800

    IDXGIinfoQueue结构体 GetMessages()方法拥有拿取DXGI消息队列里所有消息 Set()则是负责修改遍历索引的初始值

---
+++ HW3D/DxgiInfoManager.cpp
@@ -0,0 +1,70 @@
+﻿#include "DxgiInfoManager.h"
+#include "Window.h"
+#include "Graphics.h"
+#include <dxgidebug.h>
+#include <memory>
+#include <vector>
+
+#pragma comment(lib, "dxguid.lib")
+
+#define GFX_THROW_NOINFO(hrcall) if( FAILED( hr = (hrcall) ) ) throw Graphics::HrException( __LINE__,__FILE__,hr )
+
+DxgiInfoManager::DxgiInfoManager()
+{
+	// 定义1个函数签名来获取此IDXGIInfoQueue接口
+	typedef HRESULT(WINAPI* DXGIGetDebugInterface)(REFIID, void**);
+
+	// load the dll that contains the function DXGIGetDebugInterface;加载dll
+	const auto hModDxgiDebug = LoadLibraryEx("dxgidebug.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+	if (hModDxgiDebug == nullptr)
+	{
+		throw CHWND_LAST_EXCEPT();
+	}
+
+	// 在dll里查询此此IDXGIInfoQueue接口地址
+	const auto DxgiGetDebugInterface = reinterpret_cast<DXGIGetDebugInterface>(
+		reinterpret_cast<void*>(GetProcAddress(hModDxgiDebug, "DXGIGetDebugInterface"))
+		);
+	if (DxgiGetDebugInterface == nullptr)
+	{
+		throw CHWND_LAST_EXCEPT();
+	}
+
+	//获取接口
+	HRESULT hr;
+	GFX_THROW_NOINFO(DxgiGetDebugInterface(__uuidof(IDXGIInfoQueue), reinterpret_cast<void**>(&pDxgiInfoQueue)));
+}
+
+DxgiInfoManager::~DxgiInfoManager()
+{
+	if (pDxgiInfoQueue != nullptr)
+	{
+		pDxgiInfoQueue->Release();
+	}
+}
+
+void DxgiInfoManager::Set() noexcept
+{
+	//DxgiInfoManager::GetMessages()可能会获取到所有消息,所以这里设置为自上次使用set函数出现的消息即可
+	next = pDxgiInfoQueue->GetNumStoredMessages(DXGI_DEBUG_ALL);
+}
+
+std::vector<std::string> DxgiInfoManager::GetMessages() const
+{
+	std::vector<std::string> messages;
+	const auto end = pDxgiInfoQueue->GetNumStoredMessages(DXGI_DEBUG_ALL);
+	for (auto i = next; i < end; i++)
+	{
+		HRESULT hr;
+		SIZE_T messageLength;
+		// messageLength存索引消息的长度
+		GFX_THROW_NOINFO(pDxgiInfoQueue->GetMessage(DXGI_DEBUG_ALL, i, nullptr, &messageLength));
+		// 为不为0的消息分配内存
+		auto bytes = std::make_unique<byte[]>(messageLength);
+		auto pMessage = reinterpret_cast<DXGI_INFO_QUEUE_MESSAGE*>(bytes.get());
+		// 再次调用getMessage来填充内存
+		GFX_THROW_NOINFO(pDxgiInfoQueue->GetMessage(DXGI_DEBUG_ALL, i, pMessage, &messageLength));
+		messages.emplace_back(pMessage->pDescription);
+	}
+	return messages;
+}
---
+++ HW3D/DxgiInfoManager.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "GrbWin.h"
+#include <vector>
+#include <dxgidebug.h>
+#include <string>
+#include <wrl\internal.h>
+
+class DxgiInfoManager
+{
+public:
+	DxgiInfoManager();
+	~DxgiInfoManager();
+	DxgiInfoManager( const DxgiInfoManager& ) = delete;
+	DxgiInfoManager& operator=( const DxgiInfoManager& ) = delete;
+	/* DxgiInfoManager::GetMessages()可能会获取到所有消息,所以这里设置为自上次使用set函数出现的消息即可*/
+	void Set() noexcept;
+	/* 拿取DXGI消息队列里的所有消息*/
+	std::vector<std::string> GetMessages() const;
+private:
+	//For的索引开头值
+	unsigned long long next = 0u;
+	//DXGI消息队列
+	struct IDXGIInfoQueue* pDxgiInfoQueue = nullptr;
+};
