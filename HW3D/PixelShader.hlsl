8fcc861f8a3ce079e44e1a19a337e539f0278f6e
akatokikwok <guorenbin30@gmail.com>
2021-01-04 14:34:25 +0800

    着色器微调

--- HW3D/PixelShader.hlsl
+++ HW3D/PixelShader.hlsl
@@ -1,6 +1,17 @@
 //颜色也是四维浮点数
-float4 main(float3 color : Color) : SV_Target
+//float4 main(float3 color : Color) : SV_Target
+//{
+//    //return float4(1.0f, 1.0f, 1.0f, 1.0f);
+//    return float4(color,1);
+//}
+
+cbuffer CBuf
 {
-    //return float4(1.0f, 1.0f, 1.0f, 1.0f);
-    return float4(color,1);
+    float4 face_colors[6];
+};
+
+float4 main(uint tid : SV_PrimitiveID) : SV_Target
+{
+    return face_colors[tid/2];
+
 }
