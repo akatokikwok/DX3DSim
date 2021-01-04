446e993b022b0af665ba3da37100732166f87acd
akatokikwok <guorenbin30@gmail.com>
2021-01-04 14:21:21 +0800

    构造里需要带一个图元类型  D3D11_PRIMITIVE_TOPOLOGY type;//字段:图元类型

---
+++ HW3D/Topology.cpp
@@ -0,0 +1,11 @@
+﻿#include "Topology.h"
+
+Topology::Topology(Graphics& gfx, D3D11_PRIMITIVE_TOPOLOGY type)
+	:
+	type(type)
+{}
+
+void Topology::Bind(Graphics& gfx) noexcept
+{
+	GetContext(gfx)->IASetPrimitiveTopology(type);
+}
