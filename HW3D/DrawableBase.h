e0705040248606b55e875b13765169685af8cf39
akatokikwok <guorenbin30@gmail.com>
2021-01-04 15:29:57 +0800

    为了提升性能，希望每个盒子都可以共享一些Bindable对象
    在Drawable基类和其派生类中间在创建1个模板类DrawableBase
    根据模板参数差异，创建出球体、box之类的

---
+++ HW3D/DrawableBase.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "Drawable.h"
+#include "IndexBuffer.h"
+#include "Bindable.h"
+
+template<class T>
+class DrawableBase : public Drawable
+{
+public:
+	bool IsStaticInitialized() const noexcept
+	{
+		return !staticBinds.empty();
+	}
+
+	void AddStaticBind(std::unique_ptr<Bindable> bind) noexcept
+	{
+		assert("*Must* use AddIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
+		staticBinds.push_back(std::move(bind));
+	}
+
+	void AddStaticIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) noexcept
+	{
+		assert(pIndexBuffer == nullptr);
+		pIndexBuffer = ibuf.get();
+		staticBinds.push_back(std::move(ibuf));
+	}
+
+private:
+	const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept 
+	{
+		return staticBinds;
+	}
+private:
+	static std::vector<std::unique_ptr<Bindable>> staticBinds;
+};
+
+// 需要在外部再声明一下静态绑定对象
+std::vector<std::unique_ptr<Bindable>> DrawableBase<T>::staticBinds;
