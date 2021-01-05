d6fbbb90572341ebebea4dc7244cfb8d407e0d85
akatokikwok <guorenbin30@gmail.com>
2021-01-05 19:43:26 +0800

    复制original

---
+++ hw3d/App.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Window.h"
+#include "ChiliTimer.h"
+
+class App
+{
+public:
+	App();
+	// master frame / message loop
+	int Go();
+	~App();
+private:
+	void DoFrame();
+private:
+	Window wnd;
+	ChiliTimer timer;
+	std::vector<std::unique_ptr<class Drawable>> drawables;
+	static constexpr size_t nDrawables = 180;
+};
