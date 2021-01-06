e1eb83475541f72ec13f539bf03c4c35c454b113
akatokikwok <guorenbin30@gmail.com>
2021-01-06 22:34:44 +0800

    新建imgui管理类

---
+++ hw3d/ImguiManager.cpp
@@ -0,0 +1,14 @@
+﻿#include "ImguiManager.h"
+#include "imgui/imgui.h"
+
+ImguiManager::ImguiManager()
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+}
+
+ImguiManager::~ImguiManager()
+{
+	ImGui::DestroyContext();
+}
---
+++ hw3d/ImguiManager.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+class ImguiManager
+{
+public:
+	ImguiManager();
+	~ImguiManager();
+};
