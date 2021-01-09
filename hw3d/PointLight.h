76edbef918bf7ca5f2dee16d6a46fa497b164499
akatokikwok <guorenbin30@gmail.com>
2021-01-10 01:23:52 +0800

    微调

---
+++ hw3d/PointLight.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Graphics.h"
+#include "SolidSphere.h"
+#include "ConstantBuffers.h"
+
+// 点光源类
+class PointLight
+{
+public:
+	PointLight(Graphics& gfx, float radius = 0.5f);
+	void SpawnControlWindow() noexcept;
+	void Reset() noexcept;
+	// 设置灯泡位置并绘制灯泡网格
+	void Draw(Graphics& gfx) const noexcept(!IS_DEBUG);
+	// 更新点光源的常数缓存
+	void Bind(Graphics& gfx) const noexcept;
+private:
+	// 灯的像素常数缓存结构体
+	struct PointLightCBuf
+	{
+		DirectX::XMFLOAT3 pos;
+		float padding;
+	};
+private:
+	DirectX::XMFLOAT3 pos = { 0.0f,0.0f,0.0f };;	// 灯位置
+	mutable SolidSphere mesh;						// 仿灯泡形状的网格
+	mutable PixelConstantBuffer<PointLightCBuf> cbuf;// 灯的像素常数缓存
+};
---
+++ hw3d/SolidSphere.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "DrawableBase.h"
+ 
+// 灯的绘制物
+class SolidSphere : public DrawableBase<SolidSphere>
+{
+public:
+	SolidSphere(Graphics& gfx, float radius);
+	void Update(float dt) noexcept override;	
+	DirectX::XMMATRIX GetTransformXM() const noexcept override;
+	// 以参数更新点光源位置
+	void SetPos(DirectX::XMFLOAT3 pos) noexcept;
+private:
+	DirectX::XMFLOAT3 pos = { 1.0f,1.0f,1.0f };
+};
