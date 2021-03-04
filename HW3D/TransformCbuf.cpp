77ef578063a0b8e6ba9b3397639c189aef6bd4bb
askyr1m <akatokikwok@outlook.com>
2021-03-04 22:45:50 +0800

    [refactor]: 在TransformCbuf类里将 '取Transofrm结构体' 和 '更新常数缓存'的功能拆分开
    --软件工程Solid准备，软件应该对扩展开放并对修改不开放

--- HW3D/TransformCbuf.cpp
+++ HW3D/TransformCbuf.cpp
@@ -13,22 +13,29 @@ namespace Bind
 		}
 	}
 
+	/* 执行UpdateBindImpl(gfx, GetTransforms(gfx));*/
 	void TransformCbuf::Bind(Graphics& gfx) noexcept
 	{
-		// 拿到模型视图矩阵 
+		UpdateBindImpl(gfx, GetTransforms(gfx));
+	}
+		
+	void TransformCbuf::UpdateBindImpl(Graphics& gfx, const Transforms& tf) noexcept
+	{
+		pVcbuf->Update(gfx, tf); // 用入参tf以映射形式更新顶点常数缓存
+		pVcbuf->Bind(gfx); // 管线上绑定 顶点shader常数缓存
+	}
+
+	/* 在模型常数缓存里返回拿到一个结构体，它里面存有转置模型视图矩阵、转置MVP矩阵*/
+	TransformCbuf::Transforms TransformCbuf::GetTransforms(Graphics& gfx) noexcept
+	{
+		// 拿到ModelView矩阵 == 绘制物模型矩阵  * 视图矩阵
 		const auto modelView = parent.GetTransformXM() * gfx.GetCamera();
-		// 自定义1个结构体;模型矩阵的转置、和MVP的转置
-		const Transforms tf =
+		// 返回拿到1个Transforms型的结构体;模型矩阵的转置、和MVP的转置
+		return
 		{
 			DirectX::XMMatrixTranspose(modelView),
-			DirectX::XMMatrixTranspose(
-				modelView *
-
-				gfx.GetProjection()
-			)
+			DirectX::XMMatrixTranspose(modelView * gfx.GetProjection())
 		};
-		pVcbuf->Update(gfx, tf); // 用矩阵结构体常量更新顶点常数缓存
-		pVcbuf->Bind(gfx);	  // 管线上绑定常数缓存	
 	}
 
 	std::unique_ptr<VertexConstantBuffer<TransformCbuf::Transforms>> TransformCbuf::pVcbuf;
