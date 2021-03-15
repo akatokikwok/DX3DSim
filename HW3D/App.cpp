12c41f8d8268f8cc6955f5ab1f28c7ad5b715e2d
askyr1m <akatokikwok@outlook.com>
2021-03-15 23:45:04 +0800

    [feat]: 修改投影矩阵 FarZ为原来十倍

--- HW3D/App.cpp
+++ HW3D/App.cpp
@@ -48,7 +48,7 @@ App::App(const std::string& commandLine)
 	//auto c = Bind::Sampler::Resolve(wnd.Gfx());
 	//wnd.DisableCursor();//应用初始化的时候默认关闭光标
 
-	wnd.Gfx().SetProjection(dx::XMMatrixPerspectiveLH(1.0f, 9.0f / 16.0f, 0.5f, 40.0f));
+	wnd.Gfx().SetProjection(dx::XMMatrixPerspectiveLH(1.0f, 9.0f / 16.0f, 0.5f, 400.0f));
 }
 
 void App::DoFrame()
