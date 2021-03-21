f91508661b769784a24337146e41c3c55302982c
Askyr1m <akatokikwok@outlook.com>
2021-03-21 22:38:45 +0800

    [feat]:为了解决Z方向从上往下贴近地面左右方向发生问题被消除或者拉伸的BUG,采用各向异性过滤技术
    图片模糊则发生在从上往下看贴近地面的时候,那时候以Z方向压缩
    导致MIPMAP找不到一张图贴近原尺寸的;即在左右方向发生问题被消除或者拉伸
    这些问题要使用各向异性过滤技术进行消除
    它不使用选择与采样最小维度的mid Layer;它转而使用更大的更匹配最大石村的midLayer,然后迅速在z方向被压缩\平均化
    若各向异性程度越大,则GPU负担越重;最大为1比16

--- HW3D/Sampler.cpp
+++ HW3D/Sampler.cpp
@@ -9,10 +9,12 @@ namespace Bind
 		INFOMAN(gfx);
 
 		D3D11_SAMPLER_DESC samplerDesc = {};
-		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		//samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
 		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.MaxAnisotropy = D3D11_REQ_MAXANISOTROPY;
 
 		/// 为了使用MIPMAP技术,下面采样要设置LOD
 		samplerDesc.MipLODBias = 0.0f;//添加一个LOD偏差系数因子,它负责当进行LOD进行计算的时候可以使用它抵消一些
