39a17df91a08830e225dbbbfabaeb258df1f31de
Askyr1m <akatokikwok@outlook.com>
2021-03-21 21:49:11 +0800

    [fix]: 尝试修改纹理的mipmap的绑定设置
    mipmap settings cause error
    先取一组像素取这些像素的平均颜色再reduce到一个single像素以此来创建一个更小版本的像素点;并对这个小版本的进行采样
    而mipmap则是多张图每次都缩小1/4

--- HW3D/Texture.cpp
+++ HW3D/Texture.cpp
@@ -2,6 +2,7 @@
 #include "Surface.h"
 #include "GraphicsThrowMacros.h"
 #include "BindableCodex.h"
+#include <d3d11.h>
 
 namespace Bind
 {
@@ -23,15 +24,18 @@ namespace Bind
 		D3D11_TEXTURE2D_DESC textureDesc = {};
 		textureDesc.Width = s.GetWidth();
 		textureDesc.Height = s.GetHeight();
-		textureDesc.MipLevels = 1;
+		// 设置成0是表明使用一系列chain直到逐步查询到我们所需要的那一层纹理大小
+		textureDesc.MipLevels = 0;
 		textureDesc.ArraySize = 1;
 		textureDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 		textureDesc.SampleDesc.Count = 1;
 		textureDesc.SampleDesc.Quality = 0;
 		textureDesc.Usage = D3D11_USAGE_DEFAULT;
-		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		// 绑定标志不仅要设置成着色器资源;而且还需要被绑定成渲染目标,因为要使用MIPMAP的时候GPU会回写到此纹理
+		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
 		textureDesc.CPUAccessFlags = 0;
-		textureDesc.MiscFlags = 0;
+		// 很明显MiscFlags要设置成生成MIPMAPS
+		textureDesc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
 		D3D11_SUBRESOURCE_DATA sd = {};
 		sd.pSysMem = s.GetBufferPtr();
 		sd.SysMemPitch = s.GetWidth() * sizeof(Surface::Color);
@@ -40,15 +44,19 @@ namespace Bind
 			&textureDesc, &sd, &pTexture
 		));
 
-		// create the resource view on the texture
+		// 创建这个纹理的资源视图
 		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 		srvDesc.Format = textureDesc.Format;
 		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 		srvDesc.Texture2D.MostDetailedMip = 0;
-		srvDesc.Texture2D.MipLevels = 1;
+		// 设置成-1表明要是用所有的MIP
+		srvDesc.Texture2D.MipLevels = -1;
 		GFX_THROW_INFO(GetDevice(gfx)->CreateShaderResourceView(
 			pTexture.Get(), &srvDesc, &pTextureView
 		));
+
+		// 通过使用GPU渲染管线来自动生成MIP chain of 上述纹理
+		GetContext(gfx)->GenerateMips(pTextureView.Get());
 	}
 
 	void Texture::Bind(Graphics& gfx) noexcept
