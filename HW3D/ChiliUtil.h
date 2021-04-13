6312152a14c5021634d86290271082b49ce59579
Askyr1m <akatokikwok@outlook.com>
2021-04-13 23:13:23 +0800

    [feat]:token一些引用

---
+++ HW3D/ChiliUtil.h
@@ -0,0 +1,12 @@
+﻿/* 工具方法类*/
+
+#pragma once
+#include <vector>
+#include <string>
+
+/* 此函数可以token一个字符串;若存在很多引用,则可以token它们*/
+std::vector<std::string> TokenizeQuoted(const std::string& input);
+
+/* 两个方法,负责互相转化宽字符串和窄字符串*/
+std::wstring ToWide(const std::string& narrow);
+std::string ToNarrow(const std::wstring& wide);
