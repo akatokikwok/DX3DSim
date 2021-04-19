dd987624535585f981c94ecf7d25ab6e204e7d45
CHINA\gWX1027342 <akatokikwok@outlook.com>
2021-04-19 11:04:30 +0800

    [pref]:切线空间的补充介绍; 对象空间的法线 == 2 *采样后的纹理 - 1

--- HW3D/PhongPSNormalMapObject.hlsl
+++ HW3D/PhongPSNormalMapObject.hlsl
@@ -51,6 +51,15 @@ float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float2 tc
         // unpack normal data
         const float3 normalSample = nmap.Sample(splr, tc).xyz;  
         
+        /*
+        因为一个向量每个维度的取值范围在(-1, 1)，而纹理每个通道的值范围在(0, 1)，
+        因此我们需要做一个映射，即pixel = (normal + 1) / 2。即 对象空间的法线 == 2 * 采样后的纹理 - 1
+        这样，之前的法线值(0, 0, 1)实际上对应了法线纹理中RGB的值为(0.5, 0.5, 1)，而这个颜色也就是法线纹理中那大片的蓝色。
+        这些蓝色实际上说明顶点的大部分法线是和模型本身法线一样的，不需要改变。
+        总结一下就是，
+        法线纹理的RGB通道存储了在每个顶点各自的Tangent Space中的法线方向的映射值。
+        */
+        
         const float3 objectNormal = normalSample * 2.0f - 1.0f;// 拿到对象空间的法线
         viewNormal = normalize(mul(objectNormal, (float3x3) modelView)); //把法线从object space 转换到 view space同时归一化
     }    
