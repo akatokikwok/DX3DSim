66c6f48424e168b932ab4bfae6d7ad66ec228f83
Askyr1m <akatokikwok@outlook.com>
2021-04-26 21:40:53 +0800

    [feat]: 整个反射系统的初步设计理论介绍 以及此次修改 添加减少引用的宏
    Layout是一个树形结构,它用键到达树形结构上的某个节点,获取偏移值,来读写特定位置的数据.整体的布局参见P41的25分钟处的图示一个layout包含了很多layoutelement,Buffer也包含layoutelement,因为需要它来描述每个数据的作用此外,当需要访问Buffer中数据的时候(用键或者索引访问的时候)将返回一个proxy类型即Buffer中数据的代理,可以实现一些操作,比如assign赋值给某个位置的数据流程分析:最开始时候需要创建一个rawLayout(它归属于layout的一种,并包含了layoutelement;当我用索引访问layout,将拿到一个layoutelement的索引..借此你可以添加不同类型的layoutelement,形成一个树形结构
    每个layoutelemnt都有自己的类型,
    比如浮点,布尔,结构体,数组;对于布尔和矩阵来说,只需要数据类型和偏移值即可
    但是对于结构体而言,还需要额外东西;结构体成员也是一个layoutelement;至于数组,数组里面的瞬狙就只有一种类型,比如浮点数组,顾只需要存储一个layoutelement
    当创建Buffer的时候,就把RawLayout传进去,Buffer将烘焙RawLayout并计算出各种地方需要多少字节数据; Buffer所使用的树状Layout必须是已经完成的已经固定好的,这样才可以确定各个变量的偏移量
    Buffer使用[]操作符来作为索引查询特定元素并返回一个ElementRef,称之为代理类型,ElementRef实际上是对Buffer中实际数据的直接引用,同时也拥有一个指针指向Layout
    
    举例:比如有一个树形结构拥有AB子节点,B拥有CD孙子节点, ElementRef将指向这个A叶子,也就是实际数据;若打算写入值,则它将首先计算A叶子数据相对于Buffer起始位置的偏移值,并执行类型检查把值写入匹配的类型里 buf["A"] = 1.0f;
    buf["B"]["D"] = 1.0f 先访问到B子节点,再访问到B下属的D孙子节点;拿到B的ref后再检索D的REF检查是不是浮点型的,若是就写入值;若打算获取一个不存在E子节点,则系统不会抛出错误,仅仅是返回Empty类型,若出现此类情况,则推荐使用.Exist()方法来确保不为空
    
    &buff只会返回ElementRef这个代理的地址,而非真实数据的地址, 所以也需要类似的重载来拿去T型指针而非引用(这样就拿到了真正的数据地址)
    ConstElementRef是 ElementRef的Const型版本;即使用索引获取一个常数缓存时,只允许读不允许写
    
    综述,此时可以遍历整棵树,访问某节点的类型和名字

--- HW3D/App.cpp
+++ HW3D/App.cpp
@@ -70,10 +70,13 @@ App::App(const std::string& commandLine)
 
 	Dcb::Struct s(0);
 	s.Add<Dcb::Struct>("butts");
-	static_cast<Dcb::Struct&>(s["butts"]).Add<Dcb::Float3>("pubes");
+	s["butts"].AsStruct().Add<Dcb::Float3>("pubes");
+	s["butts"].AsStruct().Add<Dcb::Float>("dank");
 	Dcb::Buffer b(s);
 	b["butts"]["pubes"] = DirectX::XMFLOAT3{ 69.0f,0.0f,0.0f };
+	b["butts"]["dank"] = 420.0f;
 	dx::XMFLOAT3 v = b["butts"]["pubes"];
+	float u = b["butts"]["dank"];
 
 	//wall.SetRootTransform(dx::XMMatrixTranslation(-12.0f, 0.0f, 0.0f));//设置墙模型的根节点Transform
 	//tp.SetPos({ 12.0f,0.0f,0.0f });									//设置TestPlane绘制物的位置
