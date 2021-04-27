ad7eac73ed85c3d79724f36f504e12bc284a3aa4
Askyr1m <akatokikwok@outlook.com>
2021-04-27 22:57:28 +0800

    [feat]:拿到elementref的指针类型数据后再取地址

--- HW3D/DynamicConstant.h
+++ HW3D/DynamicConstant.h
@@ -39,6 +39,11 @@ eltype::SystemType& operator=( const eltype::SystemType& rhs ) noxnd \
 	return static_cast<eltype::SystemType&>(*this) = rhs; \
 }
 
+#define PTR_CONVERSION(eltype) \
+operator eltype::SystemType*() noxnd \
+{ \
+	return &static_cast<eltype::SystemType&>(ref); \
+}
 
 namespace Dcb
 {
@@ -173,6 +178,25 @@ namespace Dcb
 	class ElementRef
 	{
 	public:
+		class ElementPtr
+		{
+		public:
+			//ElementPtr( const ElementPtr& ) = delete;
+			ElementPtr(ElementRef& ref)
+				:
+				ref(ref)
+			{}
+
+			PTR_CONVERSION(Matrix)
+				PTR_CONVERSION(Float4)
+				PTR_CONVERSION(Float3)
+				PTR_CONVERSION(Float2)
+				PTR_CONVERSION(Float)
+				PTR_CONVERSION(Bool)
+		private:
+			ElementRef& ref;
+		};
+	public:
 		ElementRef(const LayoutElement* pLayout, char* pBytes, size_t offset)
 			:
 			offset(offset),
@@ -188,13 +212,17 @@ namespace Dcb
 			const auto& t = pLayout->T();
 			return { &t,pBytes,offset + t.GetSizeInBytes() * index };
 		}
+		ElementPtr operator&() noxnd
+		{
+			return { *this };
+		}
 
 		REF_CONVERSION(Matrix)
-			REF_CONVERSION(Float4)
-			REF_CONVERSION(Float3)
-			REF_CONVERSION(Float2)
-			REF_CONVERSION(Float)
-			REF_CONVERSION(Bool)
+		REF_CONVERSION(Float4)
+		REF_CONVERSION(Float3)
+		REF_CONVERSION(Float2)
+		REF_CONVERSION(Float)
+		REF_CONVERSION(Bool)
 	private:
 		size_t offset;
 		const class LayoutElement* pLayout;
